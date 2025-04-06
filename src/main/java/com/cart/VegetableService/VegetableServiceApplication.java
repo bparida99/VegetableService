b42158e2a5f3eae65933892943b66addd3fffae1
bparida99 <biswojitp11@gmail.com>
2025-04-06 13:32:34 +0530

    initial commit

---
+++ src/main/java/com/cart/VegetableService/VegetableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cart.VegetableService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetableServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cart/VegetableService/VegetableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart.VegetableService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegetableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
